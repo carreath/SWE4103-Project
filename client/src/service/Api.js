4cf5da8b891943c930f8c9e3b37d11f03329a3e8
brombaut <ben.rombaut@unb.ca>
2018-09-27 21:25:48 -0300

    Updating favicon and app title

--- client/src/service/Api.js
+++ client/src/service/Api.js
@@ -1,13 +1,13 @@
 import axios from 'axios'
 
 export default() => {
-    // TODO - Will have to update the base baseURL once we get a working backend
-    return axios.create({
-        baseURL: ``,
-        withCredentials: false,
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        }
-    })
+  // TODO - Will have to update the base baseURL once we get a working backend
+  return axios.create({
+    baseURL: ``,
+    withCredentials: false,
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    }
+  });
 }
