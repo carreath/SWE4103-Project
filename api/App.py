8fd2d50dfd4237cf9d1db938019e8fd672fcd428
Nathan <ntozer@unb.ca>
2018-10-10 14:58:56 -0300

    Added root resource that returns rendered index.html

--- api/App.py
+++ api/App.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_restful import Api
 from resources import *
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='', static_folder='dist', template_folder='dist')
 api = Api(app)
 
 api.add_resource(HelloWorld, '/HelloWorld')  # TODO remove eventually (keep for debugging)
@@ -11,6 +11,7 @@ api.add_resource(GameStats, "/game-stats/<game_id>")
 api.add_resource(LeagueStanding, "/standings")
 api.add_resource(PlayerStats, "/player-stats/<player_id>")
 api.add_resource(TeamRoster, "/roster/<team_id>")
+api.add_resource(Root, "/")
 
 
 if __name__ == "__main__":
--- api/resources/__init__.py
+++ api/resources/__init__.py
@@ -5,4 +5,5 @@ from resources.LeagueStanding import LeagueStanding
 from resources.PlayerStats import PlayerStats
 from resources.TeamRoster import TeamRoster
 from resources.TournamentSchedule import TournamentSchedule
+from resources.Root import Root
 #  Use above format to add new resources as needed, to allow easy importing of all resources in application
