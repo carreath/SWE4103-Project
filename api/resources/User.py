a1d435c852e2995bd7b5997e0e81ae0b4ba9a6e8
Nathan <ntozer@unb.ca>
2018-10-12 18:28:02 -0300

    Replaced direct SQL calls with a stored procedure in the User resource

--- api/resources/User.py
+++ api/resources/User.py
@@ -5,9 +5,10 @@ import json
 from common import DatabaseConnector, TokenHandler
 
 
+# Example POST request to register endpoint:
+# curl -i -H "Content-Type: application/json" -X POST -d '{"email": "ntozer@unb.ca", "password": "password", "lastName": "Tozer", "firstName": "Nathan"}' -k http://localhost:5000/api/register
 class Register(Resource):
     def post(self):
-        # curl -i -H "Content-Type: application/json" -X POST -d '{"email": "ntozer@unb.ca", "password": "password", "lastName": "Tozer", "firstName": "Nathan"}' -k http://localhost:5000/api/register
         parser = reqparse.RequestParser()
         parser.add_argument('email', type=str)
         parser.add_argument('password', type=str)
@@ -15,14 +16,13 @@ class Register(Resource):
         parser.add_argument('lastName', type=str)
         args = parser.parse_args()
 
-        print(args)
         first_name = args['firstName']
         last_name = args['lastName']
         email = args['email']
         hash_pw = pbkdf2_sha512.encrypt(args['password'], rounds=30000, salt_size=32)
 
         db_connector = DatabaseConnector()
-        if db_connector.cursor.execute('SELECT * FROM users WHERE email = "{}"'.format(email)) != 0:
+        if db_connector.cursor.execute('CALL get_user("{}");'.format(email)) != 0:
             abort(409, error='email entered is already registered', email=email)
 
         # using create_user sp to store new user
@@ -30,7 +30,7 @@ class Register(Resource):
         db_connector.conn.commit()
 
         # getting user_id to return to the frontend
-        db_connector.cursor.execute('SELECT userID FROM users WHERE email = "{}"'.format(email))
+        db_connector.cursor.execute('CALL get_user("{}");'.format(email))
         user_id = db_connector.cursor.fetchone()[0]
         db_connector.conn.close()
 
@@ -48,10 +48,11 @@ class Login(Resource):
         password = args['password']
 
         db_connector = DatabaseConnector()
-        if db_connector.cursor.execute('SELECT hash FROM users WHERE email = "{}"'.format(email)) == 0:
+        if db_connector.cursor.execute('CALL get_user("{}");'.format(email)) != 0:
             abort(404, error='email entered has not been registered')
 
         user_hash = db_connector.cursor.fetchone()[0]
+        db_connector.conn.close()
         if not pbkdf2_sha512.verify(password, user_hash):
             abort(403, error='the password entered is incorrect')
 
