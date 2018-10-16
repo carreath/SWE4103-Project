1757c0968e8c3aa9552245d32c77d11be479b011
brombaut <ben.rombaut@unb.ca>
2018-10-16 11:03:06 -0300

    Add Fredericton FC Twitter Feed component

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,39 @@
 <template>
   <div id="home">
-    HOME
+    <div id="home-left">
+      HOME
+    </div>
+    <div id="home-right">
+      <TwitterFeed/>
+    </div>
   </div>
 </template>
 
 <script>
+import TwitterFeed from '@/components/TwitterFeed.vue';
 
 export default {
   name: 'Home',
+  components: {
+    TwitterFeed,
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 #home{
+  display: flex;
+  flex-direction: row;
+  width: 100%;
 
+  #home-left{
+    width: 70%;
+  }
+
+  #home-right{
+    width: 30%;
+    display: flex;
+    justify-content: flex-end;
+  }
 }
 </style>
