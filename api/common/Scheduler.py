bcf6b1fa54e3f96d794a9c8df091f9818a46d8b6
Nathan <ntozer@unb.ca>
2018-10-30 15:25:45 -0300

    Added create_matches function to create a list of matches with no back to back matches

--- api/common/Scheduler.py
+++ api/common/Scheduler.py
@@ -25,3 +25,30 @@ assignList = {}
 while datetimeList:
     x = pairList.pop(0)
     assignList[x] = datetimeList.pop(0) #TODO implement week and time weightings
+
+
+def rotate(n):
+    return n[1:] + n[:1]
+
+
+def create_matches(team_ids):
+    num_teams = len(team_ids)
+    matches = []
+    if num_teams % 2 == 0:
+        for i in range(num_teams-1):
+            for j in range(num_teams // 2):
+                matches.append((team_ids[j], team_ids[num_teams-1-j]))
+            constant = team_ids[0]
+            team_ids.pop(0)
+            team_ids = rotate(team_ids)
+            team_ids = [constant] + team_ids
+
+    else:
+        for i in range(num_teams-1):
+            for j in range(num_teams // 2):
+                matches.append((team_ids[j], team_ids[num_teams-2-j]))
+            team_ids = rotate(team_ids)
+
+    return matches
+
+print(create_matches(teamList))
