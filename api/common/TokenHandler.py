274c1632241624236337d38e9c8a7d3942d7e0fa
Nathan <ntozer@unb.ca>
2018-11-03 13:57:36 -0300

    Added autodocs to TokenHandler.py

--- api/common/TokenHandler.py
+++ api/common/TokenHandler.py
@@ -5,10 +5,24 @@ from datetime import datetime, timedelta
 
 
 class TokenHandler:
+    """
+    TokenHandler is for handling tokens
+    """
     def __init__(self):
+        """
+        This is just the constructor
+        """
         self.secret = config.jwt_secret
 
     def create_token(self, email):
+        """
+        Creates a token from a user email
+
+        :param email: User email (String)
+
+        :return: JSON Web Token (String) with the user email encoded inside
+
+        """
         token_payload = {
             'sub': email,
             'iat': datetime.utcnow(),
@@ -17,6 +31,15 @@ class TokenHandler:
         return jwt.encode(payload=token_payload, key=self.secret, algorithm='HS256')
 
     def decode_token(self, token):
+        """
+        Decodes from a token to a user email
+
+        :param token: JSON Web Token (String)
+
+        :return: User email (String)
+
+        Aborts with a 401 if no token is passed in or the token is expired
+        """
         try:
             payload = jwt.decode(jwt=token, key=self.secret, algorithm='HS256')
             user = payload['sub']
@@ -30,5 +53,13 @@ class TokenHandler:
             abort(401, error='Invalid token, registration and/or authentication required.', authentitication=False)
 
     def refresh_token(self, token):
+        """
+        Creates a new refreshed token
+
+        :param token: JSON Web Token (String)
+
+        :return: JSON Web Token (String) with a refreshed expiration date
+
+        """
         email = self.decode_token(token)
         return self.create_token(email)
