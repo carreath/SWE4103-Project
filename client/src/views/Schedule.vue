da7ef3cc4106bcae6f1b6ad295e24af7bdf441b1
brombaut <ben.rombaut@unb.ca>
2018-11-15 21:57:35 -0400

    Schedule now updates when switching leagues

--- client/src/views/Schedule.vue
+++ client/src/views/Schedule.vue
@@ -177,6 +177,9 @@ export default {
     games() {
       this.formatGamesByLeagueIdSortedByDate();
     },
+    selectedLeagueId() {
+      this.formatGamesByLeagueIdSortedByDate();
+    },
   },
   mounted() {
     this.formatGamesByLeagueIdSortedByDate();
