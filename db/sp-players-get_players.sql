67de13f114c8f045c58bd4da2e05007e3c6f3e98
Nathan <ntozer@unb.ca>
2018-11-20 13:14:10 -0400

    Stored procedure now also returns players that have not participated in a game

--- db/sp-players-get_players.sql
+++ db/sp-players-get_players.sql
@@ -6,7 +6,12 @@ BEGIN
     SELECT p.*, CAST(SUM(g.goals) AS INT), CAST(SUM(g.cleanSheet) AS INT), CAST(SUM(g.yellowCards) AS INT), CAST(SUM(g.redCards) AS INT)
       FROM players p
       JOIN gameMembers g on p.playerID = g.playerID
-      GROUP BY p.playerID;
+      GROUP BY p.playerID
+    UNION
+    SELECT p.*, 0 AS goals, 0 AS cleanSheet, 0 AS yellowCards, 0 AS redCards
+      FROM players p
+      LEFT JOIN gameMembers g on g.playerID = p.playerID
+      WHERE g.playerID IS NULL;
 END
 
 //
