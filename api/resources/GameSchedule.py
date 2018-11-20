627c91829bca862456c6924914d10f1b0e82d117
Isayah Vidito <ividito@gmail.com>
2018-11-20 17:49:45 -0400

    League game update (#127)
    * League endpoints (put/delete)
    
    * Put endpoint for games
    
    * Delete endpoint for games
    
    * League endpoint permissions
    
    * Schedule endpoint permissions

--- api/resources/GameSchedule.py
+++ api/resources/GameSchedule.py
@@ -8,6 +8,13 @@ from common import PrivilegeHandler
 
 class LeagueSchedule(Resource):
     def get(self):
+        token = request.headers.get('Authorization')
+        if not token:
+            abort(403, error="Unauthorized Access (no token)")
+        privilege_handler = PrivilegeHandler(token)
+        if not privilege_handler.schedule_privileges():
+            abort(403, error="Unauthorized Access (invalid permissions)")
+
         db_connector = DatabaseConnector()
 
         parser = reqparse.RequestParser()
@@ -15,7 +22,7 @@ class LeagueSchedule(Resource):
         args = parser.parse_args()
 
         league_id = args['leagueID']
-        db_connector.cursor.execute('SELECT * FROM games WHERE leagueID = '+str(league_id))  # TODO filter out games from the past
+        db_connector.cursor.execute('SELECT * FROM games WHERE leagueID = '+str(league_id))
         games = db_connector.cursor.fetchall()
 
         schedule_data = []
@@ -84,9 +91,53 @@ class LeagueSchedule(Resource):
 
         return schedule, 200
 
-    def update(self):
-        # TODO update schedule information for a game
-        return
+    def put(self):
+        token = request.headers.get('Authorization')
+        if not token:
+            abort(403, error="Unauthorized Access (no token)")
+        privilege_handler = PrivilegeHandler(token)
+        if not privilege_handler.schedule_privileges():
+            abort(403, error="Unauthorized Access (invalid permissions)")
+
+        parser = reqparse.RequestParser()
+        parser.add_argument('gameID', type=int, required=True)
+        parser.add_argument('leagueID', type=int, required=True)
+        parser.add_argument('homeTeamID', type=int, required=True)
+        parser.add_argument('awayTeamID', type=int, required=True)
+        parser.add_argument('refereeID', type=int)
+        parser.add_argument('gameTime', type=str)
+        parser.add_argument('fieldName', type=str)
+
+        args = parser.parse_args()
+        query = "UPDATE games SET homeTeamID = %d, awayTeamID = %d, refereeID = %d, gameTime = '%s', fieldName = '%s' WHERE gameID = %d AND leagueID = %d" \
+                % (args['homeTeamID'], args['awayTeamID'], args['refereeID'], args['gameTime'], args['fieldName'], args['gameID'], args['leagueID'])
+        db = DatabaseConnector()
+        db.cursor.execute(query)
+        db.conn.commit()
+        return 200
+
+    def delete(self):
+        token = request.headers.get('Authorization')
+        if not token:
+            abort(403, error="Unauthorized Access (no token)")
+        privilege_handler = PrivilegeHandler(token)
+        if not privilege_handler.schedule_privileges():
+            abort(403, error="Unauthorized Access (invalid permissions)")
+        parser = reqparse.RequestParser()
+        parser.add_argument('leagueID', type=int, required=True)
+        args = parser.parse_args()
+
+        query = "SELECT gameID from gameMembers WHERE gameID in (SELECT gameID from games where leagueID = %s)" % args['leagueID']
+        db = DatabaseConnector()
+        db.cursor.execute(query)
+        res = db.cursor.fetchall()
+        if res:
+            return 400, "Cannot delete schedule were games have been recorded"
+        else:
+            query = "DELETE FROM games WHERE leagueID = %s" % args['leagueID']
+            db.cursor.execute(query)
+            db.conn.commit()
+            return 200
 
 
 class PlayerSchedule(Resource):
